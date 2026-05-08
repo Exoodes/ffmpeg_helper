cfb24e628fec675141d9648a3fc51c45486b8484
Exoodes <jakub.maca.01@gmail.com>
2026-05-08 23:08:25 +0200

    feat: ensure logging level is set to error in main function

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc, char** argv)
 {
+  av_log_set_level(AV_LOG_ERROR);
+
   CLParser parser;
   std::unique_ptr<ICommand> command = parser.parse_arguments(argc, argv);
   if(command) {
